d3e3fceb53db3e9d0b5b6dc892075ed65c4f8f85
unknown <khanyamboko70@gmail.com>
2022-07-13 08:52:13 +0200

    getitm and transactions

--- script.js
+++ script.js
@@ -11,104 +11,104 @@ const localStorageTransactions = JSON.parse(
   localStorage.getItem('transactions')
 );
 
-let transactions =
-  localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+// let transactions =
+//   localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
 
-// Add transaction
-function addTransaction(e) {
-  e.preventDefault();
+// // Add transaction
+// function addTransaction(e) {
+//   e.preventDefault();
 
-  if (text.value.trim() === '' || amount.value.trim() === '') {
-    alert('Please add a text and amount');
-  } else {
-    const transaction = {
-      id: generateID(),
-      text: text.value,
-      amount: +amount.value
-    };
+//   if (text.value.trim() === '' || amount.value.trim() === '') {
+//     alert('Please add a text and amount');
+//   } else {
+//     const transaction = {
+//       id: generateID(),
+//       text: text.value,
+//       amount: +amount.value
+//     };
 
-    transactions.push(transaction);
+//     transactions.push(transaction);
 
-    addTransactionDOM(transaction);
+//     addTransactionDOM(transaction);
 
-    updateValues();
+//     updateValues();
 
-    updateLocalStorage();
+//     updateLocalStorage();
 
-    text.value = '';
-    amount.value = '';
-  }
-}
+//     text.value = '';
+//     amount.value = '';
+//   }
+// }
 
-// Generate random ID
-function generateID() {
-  return Math.floor(Math.random() * 100000000);
-}
+// // Generate random ID
+// function generateID() {
+//   return Math.floor(Math.random() * 100000000);
+// }
 
-// Add transactions to DOM list
-function addTransactionDOM(transaction) {
-  // Get sign
-  const sign = transaction.amount < 0 ? '-' : '+';
+// // Add transactions to DOM list
+// function addTransactionDOM(transaction) {
+//   // Get sign
+//   const sign = transaction.amount < 0 ? '-' : '+';
 
-  const item = document.createElement('li');
+//   const item = document.createElement('li');
 
-  // Add class based on value
-  item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+//   // Add class based on value
+//   item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 
-  item.innerHTML = `
-    ${transaction.text} <span>${sign}${Math.abs(
-    transaction.amount
-  )}</span> <button class="delete-btn" onclick="removeTransaction(${
-    transaction.id
-  })">x</button>
-  `;
+//   item.innerHTML = `
+//     ${transaction.text} <span>${sign}${Math.abs(
+//     transaction.amount
+//   )}</span> <button class="delete-btn" onclick="removeTransaction(${
+//     transaction.id
+//   })">x</button>
+//   `;
 
-  list.appendChild(item);
-}
+//   list.appendChild(item);
+// }
 
-// Update the balance, income and expense
-function updateValues() {
-  const amounts = transactions.map(transaction => transaction.amount);
+// // Update the balance, income and expense
+// function updateValues() {
+//   const amounts = transactions.map(transaction => transaction.amount);
 
-  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+//   const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
-  const income = amounts
-    .filter(item => item > 0)
-    .reduce((acc, item) => (acc += item), 0)
-    .toFixed(2);
+//   const income = amounts
+//     .filter(item => item > 0)
+//     .reduce((acc, item) => (acc += item), 0)
+//     .toFixed(2);
 
-  const expense = (
-    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
-    -1
-  ).toFixed(2);
+//   const expense = (
+//     amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+//     -1
+//   ).toFixed(2);
 
-  balance.innerText = `R${total}`;
-  money_plus.innerText = `R${income}`;
-  money_minus.innerText = `R${expense}`;
-}
+//   balance.innerText = `R${total}`;
+//   money_plus.innerText = `R${income}`;
+//   money_minus.innerText = `R${expense}`;
+// }
 
-// Remove transaction by ID
-function removeTransaction(id) {
-  transactions = transactions.filter(transaction => transaction.id !== id);
+// // Remove transaction by ID
+// function removeTransaction(id) {
+//   transactions = transactions.filter(transaction => transaction.id !== id);
 
-  updateLocalStorage();
+//   updateLocalStorage();
 
-  init();
-}
+//   init();
+// }
 
-// Update local storage transactions
-function updateLocalStorage() {
-  localStorage.setItem('transactions', JSON.stringify(transactions));
-}
+// // Update local storage transactions
+// function updateLocalStorage() {
+//   localStorage.setItem('transactions', JSON.stringify(transactions));
+// }
 
-// Init app
-function init() {
-  list.innerHTML = '';
+// // Init app
+// function init() {
+//   list.innerHTML = '';
 
-  transactions.forEach(addTransactionDOM);
-  updateValues();
-}
+//   transactions.forEach(addTransactionDOM);
+//   updateValues();
+// }
 
-init();
+// init();
 
-form.addEventListener('submit', addTransaction);
+// form.addEventListener('submit', addTransaction);
